b7e9100fe256fc3aed7a57ac1090babe56509b4d
luisitotec2025 <blancoluisalfredo778@gmail.com>
2025-10-17 14:48:29 -0600

    cambiar config en database.py

--- database.py
+++ database.py
@@ -6,6 +6,10 @@ from sqlalchemy.orm import sessionmaker
 # Tomamos la URL de la base de datos desde Render
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# 🔥 Corrección obligatoria para Render + SQLAlchemy
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 # Configuración del motor y sesión
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
